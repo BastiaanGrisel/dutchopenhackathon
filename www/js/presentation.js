564f91b9ab53280c7277e6f13396d262c5d4e558
Sven van Hal <S.R.P.vanHal@student.tudelft.nl>
2014-09-20 17:46:10 +0200

    Integrate recipe API in presentation JS

--- www/js/presentation.js
+++ www/js/presentation.js
@@ -1,21 +1,28 @@
-$(function(){
+$(function() {
 
-	var json_example = 
-	{ "recipes":[
-		{
-			"title" : "Thaise Curry", 
-			"picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
-			"time" : 30, 
-			"url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
-		},
-		{
-			"title" : "Thaise Curry 2", 
-			"picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
-			"time" : 60, 
-			"url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
-		}]
-	};
+	/*var json_example = 
+	 { "recipes":[
+	 {
+	 "title" : "Thaise Curry", 
+	 "picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
+	 "time" : 30, 
+	 "url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
+	 },
+	 {
+	 "title" : "Thaise Curry 2", 
+	 "picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
+	 "time" : 60, 
+	 "url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
+	 }]
+	 };*/
 
-	$("#probeer_ook_eens").html(Mustache.render($("#probeer_ook_eens").html(), json_example.recipes[0]));
-	
+	$.getJSON("../server/getRecepten.php", {flightId: window.flightNumber, numberOfRecipes: 3}, function(data) {
+
+		console.log(data);
+		
+		$("#probeer_ook_eens").html(Mustache.render($("#probeer_ook_eens").html(), data[0]));
+
+	});
+
+
 });	
--- www/presentation.php
+++ www/presentation.php
@@ -21,6 +21,15 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
       <![endif]-->
+	<?php
+	if(!empty($_POST['s'])) {
+	?>
+	<script>
+		window.flightNumber = "<?php echo htmlentities($_POST['s']); ?>";
+	</script>
+	<?php
+	}
+	?>
     </head>
     <body>
 
