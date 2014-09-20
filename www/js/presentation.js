ffabbe0a36835c9d0a11b11850f71467a9bf3d4d
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2014-09-20 16:16:13 +0200

    Using Mustache templating engine

--- www/js/presentation.js
+++ www/js/presentation.js
@@ -1,32 +1,21 @@
-$( document ).ready(function() {
+$(function(){
 
-	var json_example = { "recipes":[
-	{
-		"title" : "Thaise Curry", 
-		"picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
-		"time" : 30, 
-		"url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
-	},
-	{
-		"title" : "Thaise Curry 2", 
-		"picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
-		"time" : 60, 
-		"url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
-	}
+	var json_example = 
+	{ "recipes":[
+		{
+			"title" : "Thaise Curry", 
+			"picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
+			"time" : 30, 
+			"url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
+		},
+		{
+			"title" : "Thaise Curry 2", 
+			"picture" : "http://www.ah.nl.kpnis.nl/static/recepten/img_007053_1600x560_JPG.jpg", 
+			"time" : 60, 
+			"url" : "http://www.ah.nl/allerhande/recept/R-R697352/thaise-kipcurry"
+		}]
+	};
 
-	]
-};
-
-
-console.log(json_example);
-
-console.log($(json_example.recipes).size());
-console.log(json_example.recipes[0])
-
-$.get('slides/probeer_ook_eens.php', json_example.recipes[0] 
-	).done(function(data) {
-		$('body').html(data);
-		//console.log(data);
-	});
-
-});
+	$("#probeer_ook_eens").html(Mustache.render($("#probeer_ook_eens").html(), json_example.recipes[0]));
+	
+});	
--- www/presentation.php
+++ www/presentation.php
@@ -1,31 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Presentation</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Presentation</title>
 
-		<!-- Bootstrap -->
-		<link href="css/bootstrap.css" rel="stylesheet">
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-2.1.1.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/presentation.css" rel="stylesheet">
+  
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="js/jquery-2.1.1.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/mustache.js"></script>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div id="slide"></div>
-	<script type="text/javascript" src="js/presentation.js"></script>
+      <![endif]-->
+    </head>
+    <body>
 
+      <div id="probeer_ook_eens">
+        <div class="container" style="background-image: url('{{picture}}');">
+          <div class="title prefix-title">
+            <span>Probeer ook eens...</span>
+          </div>
+          <div class="title recipe-title">
+            <span>{{title}}</span>
+          </div>
+          <div class="cooking-time">{{time}}</div>
+        </div>
+      </div>
 
+      <!-- <div id="ah_to_go">probeer ook eens</div> -->
 
-  </body>
-</html>
+      <script type="text/javascript" src="js/presentation.js"></script>
+    </body>
+    </html>
