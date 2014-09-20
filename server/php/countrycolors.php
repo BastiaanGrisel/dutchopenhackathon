018a1f5db523f9982206c44ef15dccc5aacf7cb1
Killya8D <beerdrift@gmail.com>
2014-09-20 16:11:27 +0200

    Initial commit country colors

---
+++ server/php/countrycolors.php
@@ -0,0 +1,15 @@
+<?php
+	$country = $_GET["country"];
+	
+	$colors = ["#FFFFFF", "#000000"];
+
+	echo json_encode($colors);
+
+	function getFlag($country){
+
+	}
+
+	function topColors($image){
+
+	}
+?>
