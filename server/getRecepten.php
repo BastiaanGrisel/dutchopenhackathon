1275a168ab1df595f6710302f2c6a8bbe0e4f3b8
Killya8D <beerdrift@gmail.com>
2014-09-21 03:10:24 +0200

    Track returning is now implemented in the getRecipes

--- server/getRecepten.php
+++ server/getRecepten.php
@@ -6,10 +6,12 @@ include_once("recepten.php");
 include_once("countrycolors.php");
 include_once("huemanager.php");
 include_once("bolmanager.php");
+include_once("spotify.php");
+
 
 //Getting those annoying notices and warnings out of the way
 
-// error_reporting(E_ALL);
+error_reporting(0);
 // ini_set('display_errors', 1);
 
 $flightId = $_REQUEST['flightId'];
@@ -25,6 +27,7 @@ $jsonObject->recepten = Recepten::getReceptenForCountry($countryCode, $numberOfR
 $jsonObject->colors->RGB = CountryColors::getMainColors($countryCode);
 $jsonObject->colors->HUE = CountryColors::array_RGB_TO_HUE($jsonObject->colors->RGB);
 $jsonObject->producten = BolManager::getProductsForCountry($search = Countries::getDutchName($countryCode));
+$jsonObject->track = Spotify::getTrack($countryCode);
 
 //HueManager::setColors($jsonObject->colors->HUE);
 
--- server/spotify.php
+++ server/spotify.php
@@ -1,12 +1,11 @@
 <?php
-	include_once("countrycode.php");
-
-	function getTrack($country){
+class Spotify
+{
+	public static function getTrack($country){
 		$country = Countries::getEnglishName($country);
 		$encodedJSON = file_get_contents("https://api.spotify.com/v1/search?type=track&q=" . urlencode($country));
 		$tracks = json_decode($encodedJSON)->tracks;
 		return $tracks->items[0]->preview_url;
 	}
-
-	echo getTrack($_GET["country"]);
+}
 ?>
